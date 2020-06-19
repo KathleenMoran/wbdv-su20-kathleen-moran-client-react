e22bc076cd4bc5f5ebdc3ea725a74952d4b4d74a
kathleenmoran <moran.k@husky.neu.edu>
2020-06-19 06:29:37 -0400

    Added course manager container

---
+++ src/containers/CourseManagerContainer.js
@@ -0,0 +1,117 @@
+import React from "react";
+import CourseHeadingComponent from "../components/CourseHeadingComponent";
+import CourseTableComponent from "../components/CourseTableComponent";
+import CourseGridComponent from "../components/CourseGridComponent";
+import CourseService from "../services/CourseService";
+
+const courseService = new CourseService();
+
+class CourseManagerContainer extends React.Component {
+    date
+
+    state = {
+        layout: 'table',
+        newCourseTitle: '',
+        courses: []
+    }
+
+    componentDidMount() {
+        courseService.findAllCourses()
+            .then(courses => {
+                this.setState({
+                    courses: courses
+                })
+            })
+    }
+
+    toggle = () => {
+        this.setState(prevState => ({
+                layout: prevState.layout === 'table'? 'grid': 'table'
+            })
+        )
+    }
+
+    deleteCourse = (courseToDelete) => {
+        courseService.deleteCourse(courseToDelete._id)
+            .then(() => {
+                courseService.findAllCourses()
+                    .then(courses => {
+                        this.setState({
+                            courses: courses
+                        })
+                    })
+            })
+    }
+
+    addCourse = () => {
+        this.date = new Date()
+        courseService.createCourse({
+            title: this.state.newCourseTitle,
+            dateModified: this.date.getMonth() + 1 + '/' + this.date.getDate() + '/' + this.date.getFullYear()
+        }).then(() => {
+            courseService.findAllCourses()
+                .then(courses => {
+                    this.setState({
+                        courses: courses,
+                        newCourseTitle: '',
+                    })
+                })
+        })
+    }
+
+    updateFormState = (event) => {
+        this.setState({
+            newCourseTitle: event.target.value
+        })
+    }
+
+    editCourse = (course) => {
+        this.setState(prevState => ({
+                courses: prevState.courses.map (c => {
+                    c.editing = course._id === c._id
+                    return c
+                })
+            })
+        )
+    }
+
+    updateCourse = (course) => {
+        this.date = new Date()
+        course.dateModified =  this.date.getMonth() + 1 + '/' + this.date.getDate() + '/' + this.date.getFullYear()
+        courseService.updateCourse(course._id, course)
+            .then(() => {
+                courseService.findAllCourses()
+                    .then(courses => {
+                            this.setState({
+                                courses: courses
+                            })
+                    })
+            })
+    }
+
+    render() {
+        return (
+            <div className="container-fluid p-0">
+                <CourseHeadingComponent
+                    updateFormState = {this.updateFormState}
+                    newCourseTitle = {this.state.newCourseTitle}
+                    addCourse = {this.addCourse}/>
+                {this.state.layout === 'grid' && <CourseGridComponent
+                    deleteCourse={this.deleteCourse}
+                    editCourse={this.editCourse}
+                    updateCourse={this.updateCourse}
+                    toggle={this.toggle}
+                    courses={this.state.courses}/>}
+                {this.state.layout === 'table' && <CourseTableComponent
+                    deleteCourse={this.deleteCourse}
+                    editCourse={this.editCourse}
+                    updateCourse={this.updateCourse}
+                    toggle={this.toggle}
+                    courses={this.state.courses}/>}
+            </div>
+        )
+    }
+}
+
+
+export default CourseManagerContainer
