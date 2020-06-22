49e6e3aab58908e645c97b6e7ab393f739780095
kathleenmoran <moran.k@husky.neu.edu>
2020-06-21 20:13:30 -0400

    Updated app file to use new components

--- src/App.js
+++ src/App.js
@@ -2,15 +2,10 @@
 import React from 'react';
 import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
 import CourseManagerContainer from "./containers/CourseManagerContainer";
-import CourseEditorComponent from "./components/CourseEditorComponent"
+import CourseEditorComponent from "./components/courseEditor/CourseEditorComponent"
 
 const App = () => (
-    <Router>
-        <div className="Page">
-            <Route path="/" component={CourseManagerContainer} exact/>
-            <Route path="/course" component={CourseEditorComponent}/>
-        </div>
-    </Router>
+    <CourseManagerContainer/>
 )
 
 export default App;
