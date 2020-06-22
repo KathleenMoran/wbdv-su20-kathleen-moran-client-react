9e90728dbefa1377af9f6ca2e7af84eb5486990d
kathleenmoran <moran.k@husky.neu.edu>
2020-06-21 20:02:20 -0400

    Added a topic  reducer

---
+++ src/reducers/topicReducer.js
@@ -0,0 +1,29 @@
+import {CREATE_TOPIC, DELETE_TOPIC, FIND_TOPICS_FOR_LESSON, UPDATE_TOPIC} from "../actions/topicActions";
+
+const topicReducer = (state = {topics: []}, action) => {
+    switch (action.type) {
+        case CREATE_TOPIC:
+            return {
+                topics: [
+                    ...state.topics,
+                    action.topic
+                ]
+            }
+        case DELETE_TOPIC:
+            return {
+                topics: state.topics.filter(topic => topic._id !== action.topicId)
+            }
+        case FIND_TOPICS_FOR_LESSON:
+            return {
+                topics: action.topics
+            }
+        case UPDATE_TOPIC:
+            return {
+                topics: state.topics.map(topic => topic._id === action.topicId ? action.topic : topic)
+            }
+        default:
+            return state
+    }
+}
+
+export default topicReducer
